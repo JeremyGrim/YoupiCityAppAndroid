e9b51188274402a542d7bca182c46ed2329e4611
grim <grimonpont.jeremy@gmail.com>
2017-09-05 22:31:40 +0200

    test

--- app/src/main/java/com/webgump/youpicityappandroid/home/HomeFragment.java
+++ app/src/main/java/com/webgump/youpicityappandroid/home/HomeFragment.java
@@ -3,9 +3,13 @@ package com.webgump.youpicityappandroid.home;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
 import com.special.ResideMenu.ResideMenu;
 import com.webgump.youpicityappandroid.R;
 
@@ -17,31 +21,42 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home_page, container, false);
+//        return inflater.inflate(R.layout.activity_home_page, container, false);
 //        parentView = inflater.inflate(R.layout.fragment_home, container, false);
-        //setUpViews();
 //        return parentView;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        return view;
+
+        // Give the PagerSlidingTabStrip the ViewPager
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(viewPager);
+        tabsStrip.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
 
 
 
 
     }
-
-//    private void setUpViews() {
-//        MainActivity parentActivity = (MainActivity) getActivity();
-//        resideMenu = parentActivity.getResideMenu();
-//
-//        parentView.findViewById(R.id.btn_open_menu).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                resideMenu.openMenu(ResideMenu.DIRECTION_LEFT);
-//            }
-//        });
-//
-//        // add gesture operation's ignored views
-//        FrameLayout ignored_view = (FrameLayout) parentView.findViewById(R.id.ignored_view);
-//        resideMenu.addIgnoredView(ignored_view);
-//    }
 
 }
 
